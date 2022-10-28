9c416d01db4da43947162890b07e2713daa171e1
Soloman Garcia <soloman.garcia@gmail.com>
2022-10-27 18:32:23 -0600

    add menu js interactions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,23 @@
-//!TOGGLE NAVIGATION MENU AND ICONS
+//TOGGLE NAVIGATION MENU AND ICONS
+const menuToggleButton = document.querySelector('.menu-toggle-button');
+const menuELement = document.querySelector('.menu');
 
-// !REMOVE ACTIVE CLASS FROM AND ICON ON LINK CLICK
+const toggleMenu = () => {
+  menuELement.classList.toggle('active');
+  menuToggleButton.classList.toggle('active');
+};
+
+menuToggleButton.addEventListener('click', toggleMenu);
+
+// REMOVE ACTIVE CLASS FROM AND ICON ON LINK CLICK
+const removeActiveLinkClass = e => {
+  if (e.target.classList.contains('list-link')) {
+    menuELement.classList.remove('active');
+    menuToggleButton.classList.remove('active');
+  }
+}
+
+document.addEventListener('click', removeActiveLinkClass);
 
 // !TOGGLE THEME AND STORE SELECTION WITHIN LOCAL STORAGE
 
